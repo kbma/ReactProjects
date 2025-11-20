df77817ededa357ddae420b1f0518942f907fbb9
kamel ABBASSI <abbassi.kamel@gmail.com>
2025-11-20 17:30:44 +0100

    ok

--- MiniEcommerceApp/app/src/App.jsx
+++ MiniEcommerceApp/app/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import { CartProvider } from "./context/CartContext";
-
+import "./App.css";
 export default function App() {
   return (
     <CartProvider>
