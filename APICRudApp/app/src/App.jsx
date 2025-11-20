ddde499f4d4f9af5c023ad0b9f1e634eb745cfdc
kamel ABBASSI <abbassi.kamel@gmail.com>
2025-11-20 18:10:19 +0100

    ok

---
+++ APICRudApp/app/src/App.jsx
@@ -0,0 +1,18 @@
+//src/App.jsx
+
+import './App.css';
+ 
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <h1>Dashboard Météo</h1>
+     
+      
+    </div>
+  );
+}
+ 
+export default App;
